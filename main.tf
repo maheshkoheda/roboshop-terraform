13e805f419766911836055f9efd8d65fbae37bb6
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-08-19 20:37:41 +0530

    terraform with vpc

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 module "components" {
-  source   = "git::https://github.com/maheshkoheda/tf-module-basic-test.git"
-
-  for_each = var.components
-
-  zone_id           = var.zone_id
-  security_groups   = var.security_groups
-  name              = each.value["name"]
-  instance_type     = each.value["instance_type"]
+  source   = "git::https://github.com/maheshkoheda/tf-module-vpc.git"
+  for_each = var.vpc
+  cidr = each.value["cidr"]
 }
 
