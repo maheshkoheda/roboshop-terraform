63ef860ec7a3df1115ecaf93dfaa3d924a3aaacf
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-10-18 01:00:03 -0400

    VPC Subnets routetables and associateion,gateways and nat gateways through modules concept and automated terraform with Docdb and load balancer+ Aurora DB + RDS + Elasticache + RabbitMQ +APP module frontend with Autoscaling

--- main.tf
+++ main.tf
@@ -116,6 +116,8 @@ module "app" {
     vpc_id = local.vpc_id
     subnet_ids = local.app_subnets
 
+    alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+
 
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "vpc" {
-  value = module.vpc
+output "alb" {
+  value = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
 }
